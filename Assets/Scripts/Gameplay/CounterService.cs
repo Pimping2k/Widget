382bf792a522a51defc9f5b43ad34c45162032f0
Pimping <auagfuiawg@mail.ru>
2026-02-22 22:14:03 +0200

    Added main implementation for widget-like game

---
+++ Assets/Scripts/Gameplay/CounterService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using MyPackage.Runtime.ServiceLocator_Core;
+using Services;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Gameplay
+{
+    public class CounterService : MonoBehaviour, IService
+    {
+        private IInputService _inputService;
+        
+        public int Counter { get; private set; }
+
+        public event Action<int> CounterChanged;
+        
+        private void Awake()
+        {
+            _inputService = ServiceLocator.Resolve<IInputService>();
+        }
+
+        private void Start()
+        {
+            ToggleSubscriptions(true);
+        }
+
+        private void OnDestroy()
+        {
+            ToggleSubscriptions(false);
+        }
+
+        private void ToggleSubscriptions(bool toggle)
+        {
+            if (toggle)
+            {
+                _inputService.UI.AnyClick.performed += OnAnyClickPerformed;
+            }
+            else
+            {
+                _inputService.UI.AnyClick.performed -= OnAnyClickPerformed;
+            }
+        }
+
+        private void OnAnyClickPerformed(InputAction.CallbackContext ctx)
+        {
+            PerformMainAction();
+        }
+
+        private void PerformMainAction()
+        {
+            Counter++;
+            CounterChanged?.Invoke(Counter);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UIClickCounterController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Gameplay;
+using MyPackage.Runtime.ServiceLocator_Core;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class UIClickCounterController : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _text;
+        
+        private CounterService _counterService;
+
+        private void Awake()
+        {
+            _counterService = ServiceLocator.Resolve<CounterService>();
+
+            _counterService.CounterChanged += OnCounterChanged;
+        }
+
+        private void OnDestroy()
+        {
+            _counterService.CounterChanged -= OnCounterChanged;
+        }
+
+        private void OnCounterChanged(int counter)
+        {
+            _text.text = $"I am counter : {counter}";
+        }
+    }
+}
